2a563343320dfcf8797ba962095d51f4ceb2f6d3
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-05-18 10:13:45 -0300

    Ignore first line

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import Cities from './database/cities';
 const sheets = xlsx.parse(`${__dirname}/../input/2016.xls`);
 
 sheets.map((sheet) => {
-  sheet.data.map((row) => {
+  sheet.data.filter((row, index) => {
+    return index > 0;
+  }).map((row) => {
     const stateCode = row[0];
     const stateName = row[1];
     const stateAbbr = stateNameToAbbr(stateName);
